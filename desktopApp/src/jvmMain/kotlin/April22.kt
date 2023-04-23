8ef9dbab788cbd4a2e2c590944cced105b926829
dima.avdeev <dima.avdeev@jetbrains.com>
2023-04-23 16:13:01 +0400

    update

--- desktopApp/src/jvmMain/kotlin/April22.kt
+++ desktopApp/src/jvmMain/kotlin/April22.kt
@@ -1,3 +1,19 @@
 fun main() {
-
+    var a = 1
+    var str = "Hello"
+    var list = listOf(1, 2, 3)
+    for (i in 0..10) {
+        if (i < 5) {
+            println("i < 5")
+        } else {
+            println("i >= 5")
+        }
+        when(i) {
+            0 -> println("Zero")
+            in 2..4 -> println("2..4")
+            else -> {
+                // some code
+            }
+        }
+    }
 }
