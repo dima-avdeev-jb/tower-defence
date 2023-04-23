38cacffe9be96fb79c32b84ff5595e090d0d52fa
dima.avdeev <dima.avdeev@jetbrains.com>
2023-04-23 14:59:27 +0400

    draw

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -10,9 +10,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
@@ -21,26 +21,54 @@ import kotlin.math.roundToInt
 import kotlin.math.sqrt
 import kotlin.random.Random
 
-val WORLD_SIZE = 1000f
-val ROAD_SIZE = 50f
-val TOWER_SIZE = 40f
-val ATTACK_DISTANCE = 200f
-val ENEMY_SPEED = 3f
+val WORLD_SIZE = 1000.0
+val ROAD_SIZE = 50.0
+val TOWER_SIZE = 40.0
+val ATTACK_DISTANCE = 200.0
+val ENEMY_SPEED = 3.0
 val ENEMY_HEALTH = 60
+val gameTick = mutableStateOf(0)
 
-class GameState {
-    val ticks = mutableStateOf(0)
+fun distance(tower: Tower, enemy: Enemy): Double {
+    return sqrt((tower.x - enemy.x).pow(2) + (tower.y - enemy.y).pow(2))
+}
+
+@Composable
+fun App() {
+    Box(Modifier.fillMaxSize()) {
+        GameView {
+            TowerDefenceGame()
+        }
+    }
+    Box(Modifier.fillMaxSize()) {
+
+    }
+}
+
+interface Draw {
+    fun text(x: Number, y: Number, text: String)
+    fun image(x: Number, y: Number, image: ImageBitmap)
+    fun circle(x: Number, y: Number, radius: Number, color: Color)
+    fun rectangle(x: Number, y: Number, width: Number, height: Number, color: Color)
+    fun square(centerX: Number, centerY: Number, size: Number, color: Color) {
+        rectangle(centerX.toFloat() - size.toFloat() / 2, centerY.toFloat() - size.toFloat() / 2, size, size, color)
+    }
+
+    fun line(x1: Number, y1: Number, x2: Number, y2: Number, color: Color, width: Number = 4f)
+}
+
+class TowerDefenceGame : Game() {
     var enemyScore: Int = 0
     var yourScore: Int = 0
     val enemies = mutableListOf<Enemy>()
     val towers = mutableListOf<Tower>()
 
-    fun tick() {
+    override fun tick() {
         val roadBegin = roadBlocks.first()
         if (Random.nextInt(20) == 0) {
             enemies += Enemy(
-                roadBegin.x + Random.nextDouble(ROAD_SIZE.toDouble()).toFloat(),
-                roadBegin.y + Random.nextDouble(ROAD_SIZE.toDouble()).toFloat(),
+                roadBegin.x + Random.nextDouble(ROAD_SIZE),
+                roadBegin.y + Random.nextDouble(ROAD_SIZE),
             )
         }
         val removeEnemies = mutableListOf<Enemy>()
@@ -74,92 +102,120 @@ class GameState {
             }
         }
     }
-}
 
-var gameState: GameState = GameState()
-
-fun distance(tower: Tower, enemy: Enemy): Float {
-    return sqrt((tower.x - enemy.x).pow(2) + (tower.y - enemy.y).pow(2))
-}
-
-@Composable
-fun App() {
-    Box(Modifier.fillMaxSize()) {
-        GameField()
-    }
-    Box(Modifier.fillMaxSize()) {
-        Row(Modifier.align(Alignment.BottomCenter)) {
-            Button(onClick = {
-                gameState = GameState()
-            }) {
-                Text("Restart")
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalTextApi::class)
-@Composable
-fun GameField() {
-    val rememberTextMeasurer = rememberTextMeasurer()
-    val density = LocalDensity.current
-    density.density
-    Canvas(Modifier.fillMaxSize().pointerInput(Unit) {
-        awaitPointerEventScope {
-            while (true) {
-                val event = awaitPointerEvent()
-                if (event.type == PointerEventType.Release) {
-                    val position = event.changes[0].position
-                    gameState.towers += Tower(position.x, position.y, null)
-                }
-            }
-        }
-    }) {
-        gameState.ticks.value++
-        gameState.tick()
-        drawText(
-            rememberTextMeasurer,
-            "You: ${gameState.yourScore}, Enemy: ${gameState.enemyScore}",
-            Offset(0f, 0f)
-        )
+    override fun drawGame(draw: Draw) {
+        draw.text(0, 0, "You: $yourScore, Enemy: $enemyScore")
         for (road in roadBlocks) {
-            drawRect(
+            draw.rectangle(
+                road.i * ROAD_SIZE, road.j * ROAD_SIZE,
+                ROAD_SIZE, ROAD_SIZE,
                 Color.Gray,
-                Offset(road.i * ROAD_SIZE, road.j * ROAD_SIZE),
-                Size(ROAD_SIZE, ROAD_SIZE)
             )
         }
-        for (enemy in gameState.enemies) {
+        for (enemy in enemies) {
             val enemyColor =
                 Color(
                     red = (1f - enemy.health.toFloat() / ENEMY_HEALTH).coerceIn(0f, 1f),
                     green = 0f,
                     blue = 0f
                 )
-            drawCircle(enemyColor, radius = 10f, center = Offset(enemy.x, enemy.y))
+            draw.circle(enemy.x, enemy.y, 20, enemyColor)
         }
-        for (tower in gameState.towers) {
-            drawRect(
-                color = Color.Blue,
-                topLeft = Offset(tower.x - TOWER_SIZE / 2, tower.y - TOWER_SIZE / 2),
-                size = Size(TOWER_SIZE, TOWER_SIZE)
+        for (tower in towers) {
+            draw.square(
+                tower.x,
+                tower.y,
+                TOWER_SIZE,
+                Color.Blue
             )
             val attack = tower.attack
             if (attack != null) {
-                drawLine(
+                draw.line(
+                    tower.x, tower.y,
+                    attack.x, attack.y,
                     Color.Green,
-                    start = Offset(tower.x, tower.y),
-                    end = Offset(attack.x, attack.y),
-                    strokeWidth = 4f
                 )
             }
         }
-        drawImage(getImage("castle.png"), Offset(450f, 950f))
+        draw.image(450f, 950f, getImage("castle.png"))
+    }
+
+    override fun onClick(x: Double, y: Double) {
+        towers += Tower(x, y, null)
     }
 }
 
-class Enemy(var x: Float, var y: Float, var health: Int = ENEMY_HEALTH)
-class Tower(val x: Float, val y: Float, var attack: Enemy?)
+open class Game {
+    open fun tick() {
+
+    }
+
+    open fun drawGame(draw: Draw) {
+
+    }
+
+    open fun onClick(x: Double, y: Double) {
+
+    }
+}
+
+@OptIn(ExperimentalTextApi::class)
+@Composable
+fun GameView(getGame: () -> Game) {
+    val game = remember { mutableStateOf(getGame()) }
+    val textMeasure = rememberTextMeasurer()
+    Box(Modifier.fillMaxSize()) {
+        Canvas(Modifier.fillMaxSize().pointerInput(Unit) {
+            awaitPointerEventScope {
+                while (true) {
+                    val event = awaitPointerEvent()
+                    if (event.type == PointerEventType.Release) {
+                        val position = event.changes[0].position
+                        game.value.onClick(
+                            position.x.toDouble(), position.y.toDouble()
+                        )
+                    }
+                }
+            }
+        }) {
+            gameTick.value++
+            game.value.tick()
+            game.value.drawGame(object : Draw {
+                override fun text(x: Number, y: Number, text: String) {
+                    drawText(textMeasure, text, Offset(x.toFloat(), y.toFloat()))
+                }
+
+                override fun image(x: Number, y: Number, image: ImageBitmap) {
+                    drawImage(image, topLeft = Offset(x.toFloat(), y.toFloat()))
+                }
+
+                override fun circle(x: Number, y: Number, radius: Number, color: Color) {
+                    drawCircle(color, radius.toFloat(), Offset(x.toFloat(), y.toFloat()))
+                }
+
+                override fun rectangle(x: Number, y: Number, width: Number, height: Number, color: Color) {
+                    drawRect(color, topLeft = Offset(x.toFloat(), y.toFloat()), size = Size(width.toFloat(), height.toFloat()))
+                }
+
+                override fun line(x1: Number, y1: Number, x2: Number, y2: Number, color: Color, width: Number) {
+                    drawLine(color, Offset(x1.toFloat(), y1.toFloat()), Offset(x2.toFloat(), y2.toFloat()), strokeWidth = width.toFloat())
+                }
+            })
+        }
+
+        Row(Modifier.align(Alignment.BottomCenter)) {
+            Button(onClick = {
+                game.value = getGame()
+            }) {
+                Text("Restart")
+            }
+        }
+    }
+
+}
+
+class Enemy(var x: Double, var y: Double, var health: Int = ENEMY_HEALTH)
+class Tower(val x: Double, val y: Double, var attack: Enemy?)
 data class RoadBlock(val i: Int, val j: Int)
 
 val RoadBlock.x get() = i * ROAD_SIZE
