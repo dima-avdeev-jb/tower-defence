739e991fdd82fbc4238ecb972918b401f5cfb41a
dima.avdeev <dima.avdeev@jetbrains.com>
2023-04-23 22:58:31 +0400

    April23_1

--- desktopApp/src/main/kotlin/April23_1.kt
+++ desktopApp/src/main/kotlin/April23_1.kt
@@ -1,3 +1,41 @@
 fun April23_1() {
-    println("april 23")
+    val list: List<String> = listOf("a", "b", "c")
+    println(list) // [a, b, c]
+                   // 0  1  2 - indexes
+    println(list.get(0))
+    println(list.size)
+    for (element in list) {
+        println(element)
+        // a
+        // b
+        // c
+    }
+    for (i in list.indices) { // пройти по индексам
+        println(i)
+        // 0
+        // 1
+        // 2
+    }
+    for (i in 0..(list.size - 1)) {
+        println(i)
+        // 0
+        // 1
+        // 2
+    }
+
+    val mutableList: MutableList<Int> = mutableListOf(10, 20, 30)
+    mutableList.set(0, 5)
+    println(mutableList) // [5, 20, 30]
+    mutableList.add(40) //  [5, 20, 30, 40]
+    mutableList.remove(20) // [5, 30, 40]
+    mutableList.removeAt(0) // [30, 40]
 }
+
+//fun privet(name: String, age: Int): String {
+//    var old: String = ""
+//    if (age < 5) {
+//        old = "baby"
+//    }
+//    val c = "Hello $name $old, you are $age years"
+//    return c
+//}
