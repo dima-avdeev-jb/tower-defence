63d574d3b8baecaf8ee9917dc8ce89b419e5ffd5
dima.avdeev <dima.avdeev@jetbrains.com>
2023-04-23 15:40:05 +0400

    remove iphone

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -7,26 +7,7 @@ plugins {
 
 kotlin {
     android()
-
     jvm("desktop")
-
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-
-    cocoapods {
-        version = "1.0.0"
-        summary = "Some description for the Shared Module"
-        homepage = "Link to the Shared Module homepage"
-        ios.deploymentTarget = "14.1"
-        podfile = project.file("../iosApp/Podfile")
-        framework {
-            baseName = "shared"
-            isStatic = true
-        }
-        extraSpecAttributes["resources"] = "['src/commonMain/resources/**', 'src/iosMain/resources/**']"
-    }
-
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -44,15 +25,7 @@ kotlin {
                 api("androidx.core:core-ktx:1.9.0")
             }
         }
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
+
         val desktopMain by getting {
             dependencies {
                 implementation(compose.desktop.common)
