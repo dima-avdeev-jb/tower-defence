9d3dc91d14c5e12b0b4812d4f29ca1d45da7acb8
dima.avdeev <dima.avdeev@jetbrains.com>
2023-04-24 22:19:34 +0400

    paint

--- shared/src/commonMain/kotlin/PainGame.kt
+++ shared/src/commonMain/kotlin/PainGame.kt
@@ -7,7 +7,12 @@ class PaintGame : BaseGame() {
     }
 
     override fun drawGame(draw: Draw) { // draw - рисовать
-        draw.circle(100, 100, 20, Color.Black)
+        draw.circle(100, 100, 50, Color.Black)
+        draw.square(100, 300, 100, Color.Green)
+        draw.rectangle(100, 500, 200, 100, Color.Red)
+        draw.line(300, 300, 700, 400, Color.Black)
+        draw.text(0, 0, "I am Text")
+        draw.image(100, 600, getImage("castle.png"))
     }
 
     override fun onClick(x: Double, y: Double) {
