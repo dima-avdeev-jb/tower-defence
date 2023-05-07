9072e9a39b27c7b056717f4ec1e4833167261d34
grigory <119728682+loxcds@users.noreply.github.com>
2023-05-07 14:56:02 +0300

    fdrgreweqwfdasdesfdsfdsdsfsdasd

--- desktopApp/src/main/kotlin/May07.kt
+++ desktopApp/src/main/kotlin/May07.kt
@@ -1,3 +1,34 @@
 fun May07() {
+//    val a = 123
+//    println(a)
+    val l = listOf<Int>(1, 2, 3)
+//    println(l)
+//
+//    for (element in l) {
+//        println(element)
+//    }
 
+    for (index in l.indices) {
+        println(l.get(index))
+    }
+    PrintName("Dima", 12)
+    val Result = CanDrink(18)
+    if(Result == true){
+        println("You can")
+    }else{
+        println("No you can't")
+    }
+    println(Result)
+}
+
+fun PrintName(Name: String, age: Int) {
+    println("Hello $Name, your age is: $age")
+}
+
+fun CanDrink(Age: Int): Boolean {
+    if (Age > 17) {
+        return true
+    } else {
+        return false
+    }
 }
